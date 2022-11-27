a08fc793d74d93f6e459f6281c1c419e7d7b7e3e
guilleee <88690005+guilleee@users.noreply.github.com>
2022-11-27 16:41:40 -0300

    payment mejorado

--- src/controllers/payment.js
+++ src/controllers/payment.js
@@ -1,11 +1,6 @@
-
-
 const payment = async (req, res) => {
-    console.log("ENTRE ACA");
     numberi = Math.floor(Math.random() * 1800) + 1200;
     valid = Math.floor(Math.random() * 100);
-    console.log("numberi: " + numberi);
-    console.log("valid: " + valid);
     if (valid >= 90) {
         approvedi = false;
     }
